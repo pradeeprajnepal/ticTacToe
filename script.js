390244f24363041ce816d2ba872e126e185f41fb
Pradeep Raj Nepal <pradeepxwon@gmail.com>
2022-02-06 00:09:23 +0545

    win-draw-restart-functionality

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 const x_class= "x";
 const circle_class="circle";
+const winCombinations=[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
 const cellElement= document.querySelectorAll("[data-cell]");
+const winningMessageText= document.querySelector("[data-winning-message-text]");
+const winningMessage= document.getElementById("winningMessage");
+const restartButton= document.getElementById("restart-button");
 const board= document.getElementById("board");
-
 let circleTurn
+
+restartButton.addEventListener("click",startGame);
 
 startGame();
 
 function startGame(){
     circleTurn=false;
     cellElement.forEach(cell=>{
+        cell.classList.remove(x_class);
+        cell.classList.remove(circle_class);
+        cell.removeEventListener("click",handleClick)
         cell.addEventListener("click",handleClick,{once:true});
+        
     })
     addHoverClass();
+    winningMessage.classList.remove("show");
 }
 
 function handleClick(e){
@@ -23,13 +42,18 @@ function handleClick(e){
     placeMark(cell,currentClass);
 
     //check win
-    //check draw
-    //switch turn
-
-    switchTurn();
-
-    //hover effect
-    addHoverClass()
+    if(checkWin(currentClass)){
+        endgame(false);
+    }else if(isDraw()){//check draw
+        endgame(true);
+    }else{
+        //switch turn
+        switchTurn();
+        //hover effect
+        addHoverClass();
+    }
+    
+    
 }
 
 function placeMark(cell,currentClass){
@@ -49,4 +73,27 @@ function addHoverClass(){
     }else{
         board.classList.add(x_class);
     }
+}
+
+function checkWin(currentClass){
+    return winCombinations.some(combination=>{
+        return combination.every(index=>{
+            return cellElement[index].classList.contains(currentClass);
+        })
+    })
+}
+
+function endgame(draw){
+    if(draw){
+        winningMessageText.innerText= "Draw";
+    }else{
+        winningMessageText.innerText= `${circleTurn ? "O's" : "X's"} Wins!`;
+    }
+    winningMessage.classList.add("show");
+}
+
+function isDraw(){
+    return [...cellElement].every(cell=>{
+        return cell.classList.contains(x_class) || cell.classList.contains(circle_class);
+    })
 }
